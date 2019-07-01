eacf4e8be1c587e79e879143e7365504e542c59d
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-07-01 16:43:21 +0200

    Update test03.py

--- test03.py
+++ test03.py
@@ -1,4 +1,38 @@
+
 '''
+actually below script is the main script which i want to execute
+just i want to solve the IF statement in this script otwise my script
+is working on my side
+-------------------------------------------------------------------
+import json for json stream
+request module is for request
+webbrowser for open URL
+--------------------------------------------------------------------
+'''        
+import json 
+import requests
+import webbrowser
+r = requests.get('http://rhbsr001.koe.wige.local/v1/Data?id=4711')
+packages_json =r.json()
+packages_str = json.dumps(packages_json, indent=2)
+
+print(packages_str)
+print(packages_json["stream"])
+
+url_num_list = ['stream']
+
+for urls in url_num_list:
+    address = (packages_json["stream"])
+    if url_num_list == url_num_list:
+        new=2;
+        webbrowser.open(address,new=new)
+        break
+        
+    elif url_num_list != url_num_list:
+        new=2;
+        webbrowser.open(address,new=new);
+'''
+Main Problme 
 I want to write a program for videos looping for example: 
 My loop check the video URL if the video URL is == (equal to) 
 video URL play the video and loop this video again and again.
@@ -22,43 +56,10 @@ for urls in url:
         #check loop again 
         #if URL (not equal) play default video
         
-    '''
-actually below script is the main script which i want to execute
-just i want to solve the IF statement in this script otwise my script
-is working on my side
-
-import json for json stream
-request module is for request
-webbrowser for open URL
-
 
         
-import json 
-import requests
-import webbrowser
-
-r = requests.get('http://rhbsr001.koe.wige.local/v1/Data?id=4711')
-packages_json =r.json()
-packages_str = json.dumps(packages_json, indent=2)
-
-print(packages_str)
-print(packages_json["stream"])
-
-url_num_list = ['stream']
-
-for urls in url_num_list:
-    address = (packages_json["stream"])
-    if url_num_list == url_num_list:
-        new=2;
-        webbrowser.open(address,new=new)
-        break
         
-    elif url_num_list != url_num_list:
-        new=2;
-        webbrowser.open(address,new=new);
-        
-        
-    '''
+    
         
         
     
