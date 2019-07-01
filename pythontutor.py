896d5491cb2f25fdc44978e265dd97bad33bcbb6
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-07-01 14:31:25 +0200

    Update pythontutor.py

--- pythontutor.py
+++ pythontutor.py
@@ -1,25 +1,19 @@
 '''
-i am writing a Python Program, that get the URL store in json stream 
-this URL play .mp4 video which on cloud(API). 
-When i change the video id then current video will be stop and
-play new video corresponding with the id, that will happened after first video 
-will end. Check if video id 
-will same play same video otherwise play corresponding id video.
-how to write loop that paly video till end and check above condition.
+I want to write a program for videos looping for example: 
+My loop check the video URL if the video URL is == (equal to) 
+video URL play the video and loop this video again and again.
+If the video URL is != (not equal to Video URL) Current video URL 
+stop and start the default video. 
 
-importatn: how to write simple loop ? that play video till end 
-and check if video url will same play same video if url is not same 
-play new video till end. just simple loop. 
+in this python script, get the URL from json stream which are store in 
+one of aws.
+1:requests module use for get URL 
+2:webbrowsr use for open URL
+This is my code  
 '''
-
-
 import json 
 import requests
 import webbrowser
-import time
-from schedule import * 
-import schedule
-import subprocess
 
 r = requests.get('http://rhbsr001.koe.wige.local/v1/Data?id=4711')
 packages_json =r.json()
@@ -28,25 +22,16 @@ packages_str = json.dumps(packages_json, indent=2)
 print(packages_str)
 print(packages_json["stream"])
 
-r = r
-while r != r:
-    print("The End")
-    break
-else:
-    new=2;
-    url=(packages_json["stream"])
-    webbrowser.open(url,new=new);
-    
+url_num_list = ['stream']
 
-def job():
-    new=2;
-    url=(packages_json["stream"])
-    webbrowser.open(url,new=new);
-#    subprocess.call("taskkill /IM chrome.exe")
-    
-    
-#schedule.every(20).seconds.do(job)
-
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+for urls in url_num_list:
+    address = (packages_json["stream"])
+    if url_num_list == url_num_list:
+        new=2;
+        webbrowser.open(address,new=new)
+        break
+        
+    elif url_num_list != url_num_list:
+        new=2;
+        webbrowser.open(address,new=new);
+         
