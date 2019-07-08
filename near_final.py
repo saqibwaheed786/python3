f6c6f02095712e412451fea27780cca4d20a49d6
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-07-08 17:11:17 +0200

    Create near_final.py

---
+++ near_final.py
@@ -0,0 +1,33 @@
+import json 
+import requests
+import webbrowser
+import sched, time
+import subprocess
+
+r = requests.get('http://rhbsr001.koe.wige.local/v1/Data?id=4711')
+packages_json = r.json()
+packages_str = json.dumps(packages_json, indent=2)
+
+print(packages_str)
+print(packages_json["stream"])
+
+url = packages_json["stream"]
+
+s = sched.scheduler(time.time, time.sleep)
+address = (packages_json["stream"])
+webbrowser.open(address,new=2)
+
+while True:
+    address = (packages_json["stream"])
+    time.sleep(1)
+    r1 = requests.get('http://rhbsr001.koe.wige.local/v1/Data?id=4711')
+    packages_json = r1.json()
+    url1 = packages_json["stream"]
+    changed= url != url1
+    if changed:
+        address = (packages_json["stream"])
+        subprocess.call("taskkill /IM chrome.exe")
+        webbrowser.open(address,new=2)
+        url=url1
+    else:
+        print('')
