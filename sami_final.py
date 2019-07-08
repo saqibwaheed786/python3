d4da866c56c75084fdc4debaf619a407f518a5c4
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-07-08 15:53:01 +0200

    Create sami_final.py

---
+++ sami_final.py
@@ -0,0 +1,38 @@
+import json 
+import requests
+import webbrowser
+import sched, time
+import subprocess
+
+r = requests.get('http://rhbsr001.koe.wige.local/v1/Data?id=4711')
+packages_json = r.json()
+packages_str = json.dumps(packages_json, indent=2)
+
+print(packages_str)
+print(packages_json["stream"])
+
+url = packages_json["stream"]
+
+s = sched.scheduler(time.time, time.sleep)
+
+# def changeStreamUrl(json_file):
+#     return "Magic"
+    
+while True:
+    address = (packages_json["stream"])
+    # changeStreamUrl(url)
+    time.sleep(1)
+    r1 = requests.get('http://rhbsr001.koe.wige.local/v1/Data?id=4711')
+    packages_json = r1.json()
+    url1 = packages_json["stream"]
+    changed= url != url1
+    if changed:
+        address = (packages_json["stream"])
+        subprocess.call("taskkill /IM chrome.exe")
+        webbrowser.open(address,new=2)
+        url=url1
+    else:
+        print('doing nothing')
+        # time.sleep(1)
+        # webbrowser.open(address,new=new) # this may be the problem
+        
